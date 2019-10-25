73db8fa5e946758c9d38ee74c934a28af134fc5b
Chris H <chrissy14az@hotmail.co.uk>
2019-10-25 12:28:05 +0100

    Setting up task - INCOMPLETE

---
+++ src/activity1_scannertest/ScannerExercise.java
@@ -0,0 +1,22 @@
+/*
+ * Practice using the Scanner to perform tasks on user inputs
+ * @author st20155985
+ */
+
+package activity1_scannertest;
+
+public class ScannerExercise
+{
+
+    public static void main(String[] args)
+    {
+
+        /* TASK
+         * Create a new class of your own that uses the Scanner to collect the names and
+         * ages of four people and calculates the average age of those four people.
+         * e.g. "The average age of person-a, person-b, person-c and person-d is average-age".
+         */
+
+    }
+
+}
