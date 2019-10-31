244b97415f39a7f753a21f517a4ce23a1999a376
StarTrekManiac2 <chrissy14az@hotmail.co.uk>
2019-10-31 10:04:59 +0000

    - Still working on 'Lottery' activity - semi-broken but refactored

--- src/activity3_numgenerator/Lottery.java
+++ src/activity3_numgenerator/Lottery.java
@@ -8,7 +8,7 @@ public class Lottery
 
     /**
         I need to verify that the user inputs birthday numbers from 1 to 31.
-        I can do this by considering a Do-While Loop that repeatedly asks UP TO 6 times for a birthday
+        I can do this by considering a FOR Loop that repeatedly asks UP TO 6 times for a birthday
         number for the lottery ticket if desired.
 
         I.E.
@@ -44,172 +44,48 @@ public class Lottery
         int numE = 0;
         int numF = 0;
 
-        // Declaring birthday number variables
-        int birthdayNumA, birthdayNumB, birthdayNumC, birthdayNumD, birthdayNumE, birthdayNumF;
-
         // Initializing array of above number variables
         int[] lotteryNumbers = { numA, numB, numC, numD, numE, numF };
-
 
 
         /************************************
          * PROGRAM FUNCTIONALITY
          ************************************/
 
-        // Ask whether or not to include birthday numbers
-        System.out.println("Type 'Y' if you wish to include birthdays (1 to 31)?");
-        String response = s.next();
+        // Count for each time the user is asked for birthday
+        int count = 1;
 
-        // If YES to including birthday numbers
-        if (response.equals("Y") || response.equals("y"))
+        // For-Loop for asking the user in each iteration of the lottery number array
+        for (int i=0; i<lotteryNumbers.length; i++)
         {
-            // Ask how many birthday numbers to include
-            System.out.println("How many birthdays do you want to include (1-6)?");
-            int amount = s.nextInt();
+            // Ask the user for input
+            System.out.println("Do you want to add a birthday number of 1 to 31? [" + count + "]");
 
-            // Perform birthday inputs depending on amount desired above
-            if (amount == 1)
+            // IF response is a form of 'Yes', ask for the user to input and apply it to current iteration
+            if ((s.next() == "Y") || (s.next() == "y") || (s.next() == "Yes") || (s.next() == "yes"))
             {
-                System.out.println("Birthday Number: ");
-                birthdayNumA = s.nextInt();
-
-                // Apply to lottery array
-                lotteryNumbers[0] = birthdayNumA;
-
-                // Randomize remaining numbers
-                for (int i = 1; i < lotteryNumbers.length; i++)
-                {
-                    lotteryNumbers[i] = (int) (Math.random() * (43 - 1) + 1);
-                }
+                System.out.println("Type Birthday #" + i);
+                lotteryNumbers[i] = s.nextInt();
             }
-            else if (amount == 2)
-            {
-                System.out.println("Birthday Numbers: ");
-                birthdayNumA = s.nextInt();
-                birthdayNumB = s.nextInt();
-
-                // Apply to lottery array
-                lotteryNumbers[0] = birthdayNumA;
-                lotteryNumbers[1] = birthdayNumB;
-
-                // Randomize remaining numbers
-                for (int i = 2; i < lotteryNumbers.length; i++)
-                {
-                    lotteryNumbers[i] = (int) (Math.random() * (43 - 1) + 1);
-                }
-            }
-            else if (amount == 3)
-            {
-                System.out.println("Birthday Numbers: ");
-                birthdayNumA = s.nextInt();
-                birthdayNumB = s.nextInt();
-                birthdayNumC = s.nextInt();
-
-                // Apply to lottery array
-                lotteryNumbers[0] = birthdayNumA;
-                lotteryNumbers[1] = birthdayNumB;
-                lotteryNumbers[2] = birthdayNumC;
-
-                // Randomize remaining numbers
-                for (int i = 3; i < lotteryNumbers.length; i++)
-                {
-                    lotteryNumbers[i] = (int) (Math.random() * (43 - 1) + 1);
-                }
-            }
-            else if (amount == 4)
-            {
-                System.out.println("Birthday Numbers: ");
-                birthdayNumA = s.nextInt();
-                birthdayNumB = s.nextInt();
-                birthdayNumC = s.nextInt();
-                birthdayNumD = s.nextInt();
-
-                // Apply to lottery array
-                lotteryNumbers[0] = birthdayNumA;
-                lotteryNumbers[1] = birthdayNumB;
-                lotteryNumbers[2] = birthdayNumC;
-                lotteryNumbers[3] = birthdayNumD;
-
-                // Randomize remaining numbers
-                for (int i = 4; i < lotteryNumbers.length; i++)
-                {
-                    lotteryNumbers[i] = (int) (Math.random() * (43 - 1) + 1);
-                }
-            }
-            else if (amount == 5)
-            {
-                System.out.println("Birthday Numbers: ");
-                birthdayNumA = s.nextInt();
-                birthdayNumB = s.nextInt();
-                birthdayNumC = s.nextInt();
-                birthdayNumD = s.nextInt();
-                birthdayNumE = s.nextInt();
-
-                // Apply to lottery array
-                lotteryNumbers[0] = birthdayNumA;
-                lotteryNumbers[1] = birthdayNumB;
-                lotteryNumbers[2] = birthdayNumC;
-                lotteryNumbers[3] = birthdayNumD;
-                lotteryNumbers[4] = birthdayNumE;
-
-                // Randomize remaining numbers
-                for (int i = 5; i < lotteryNumbers.length; i++)
-                {
-                    lotteryNumbers[i] = (int) (Math.random() * (43 - 1) + 1);
-                }
-            }
-            else if (amount == 6)
-            {
-                System.out.println("Birthday Numbers: ");
-                birthdayNumA = s.nextInt();
-                birthdayNumB = s.nextInt();
-                birthdayNumC = s.nextInt();
-                birthdayNumD = s.nextInt();
-                birthdayNumE = s.nextInt();
-                birthdayNumF = s.nextInt();
-
-                // Apply to lottery array - no need to randomize afterward
-                lotteryNumbers[0] = birthdayNumA;
-                lotteryNumbers[1] = birthdayNumB;
-                lotteryNumbers[2] = birthdayNumC;
-                lotteryNumbers[3] = birthdayNumD;
-                lotteryNumbers[4] = birthdayNumE;
-                lotteryNumbers[5] = birthdayNumF;
-            }
+            // ELSE IF not, randomize current iteration's number instead
             else
             {
-                // Terminates the program due to error
-                System.out.println("You did not enter a valid number of birthdays to include (1 to 6)!");
-                System.exit(1);
+                System.out.println("Number has been randomized");
+                lotteryNumbers[i] = (int) (Math.random() * (43 - 1) + 1);
             }
 
-            // Sort array into ascending order
-            Arrays.sort(lotteryNumbers);
-
-            // Output lottery numbers
-            for (int i=0; i<lotteryNumbers.length; i++)
-            {
-                System.out.println(lotteryNumbers[i]);
-            }
+            // Increment the birthday counter and increment the iteration to continue
+            count++;
+            i++;
         }
 
-        // IF NO to including birthday numbers
-        else
+        // Sort the array values (ascending order)
+        Arrays.sort(lotteryNumbers);
+
+        // Output each array value to console
+        for (int j=0; j<lotteryNumbers.length; j++)
         {
-            // Declaring all 6 random numbers (1 to 42)
-            for (int i=0; i<lotteryNumbers.length; i++)
-            {
-                lotteryNumbers[i] = (int) (Math.random() * (43-1) +1);
-            }
-
-            // Sort array into ascending order
-            Arrays.sort(lotteryNumbers);
-
-            // Output lottery numbers
-            for (int i=0; i<lotteryNumbers.length; i++)
-            {
-                System.out.println(lotteryNumbers[i]);
-            }
+            System.out.println(lotteryNumbers[j]);
         }
 
     }
