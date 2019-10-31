89af5c4c953d4a906b77bcd60214067f16f084a9
StarTrekManiac2 <chrissy14az@hotmail.co.uk>
2019-10-31 10:48:59 +0000

    - Still working on 'Lottery' activity - extra iteration fix

--- src/activity3_numgenerator/Lottery.java
+++ src/activity3_numgenerator/Lottery.java
@@ -7,50 +7,22 @@ public class Lottery
 {
 
     /**
-        I need to verify that the user inputs birthday numbers from 1 to 31.
-        I can do this by considering a FOR Loop that repeatedly asks UP TO 6 times for a birthday
-        number for the lottery ticket if desired.
-
-        I.E.
-            "Do you want to add a birthday number of 1 to 31? [1]"
-                - yes
-                - "Number:"
-                - 20
-                - if not 1-31, randomize instead and move on (maybe add a message to say so)
-            "Do you want to add a birthday number of 1 to 31? [2]"
-
-            ...
-
-            "Do you want to add a birthday number of 1 to 31? [4]"
-                - no
-                - Randomizes the rest if applicable
+     *
+     *  TO-DO
+     *
+     *  - I need to verify that the user inputs birthday numbers from 1 to 31.
+     *  - There is an error in that it ignores the IF statement and skips to the ELSE despite correct condition.
+     *
      **/
 
     public static void main(String[] args)
     {
 
-        /************************************
-         * SETUP
-         ************************************/
-
         // Initialize input Scanner
         Scanner s = new Scanner(System.in);
 
-        // Declaring number variables
-        int numA = 0;
-        int numB = 0;
-        int numC = 0;
-        int numD = 0;
-        int numE = 0;
-        int numF = 0;
-
-        // Initializing array of above number variables
-        int[] lotteryNumbers = { numA, numB, numC, numD, numE, numF };
-
-
-        /************************************
-         * PROGRAM FUNCTIONALITY
-         ************************************/
+        // Initializing integer array of 6 elements
+        int[] lotteryNumbers = new int[6];
 
         // Count for each time the user is asked for birthday
         int count = 1;
@@ -60,9 +32,10 @@ public class Lottery
         {
             // Ask the user for input
             System.out.println("Do you want to add a birthday number of 1 to 31? [" + count + "]");
+            String response = s.next();
 
             // IF response is a form of 'Yes', ask for the user to input and apply it to current iteration
-            if ((s.next() == "Y") || (s.next() == "y") || (s.next() == "Yes") || (s.next() == "yes"))
+            if (response == "y")
             {
                 System.out.println("Type Birthday #" + i);
                 lotteryNumbers[i] = s.nextInt();
@@ -76,7 +49,6 @@ public class Lottery
 
             // Increment the birthday counter and increment the iteration to continue
             count++;
-            i++;
         }
 
         // Sort the array values (ascending order)
