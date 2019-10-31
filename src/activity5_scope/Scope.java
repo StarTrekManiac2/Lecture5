fd50e9232863b4d80b169dd336f1fd6b3b7cb70f
StarTrekManiac2 <chrissy14az@hotmail.co.uk>
2019-10-31 11:11:04 +0000

    Finished 'Scope' activity

---
+++ src/activity5_scope/Scope.java
@@ -0,0 +1,30 @@
+package activity5_scope;
+
+public class Scope
+{
+
+    public static void main(String[] args)
+    {
+
+        // Local Variable access example
+        String text = "This is 'TEXT' - it is local to the main method";
+        System.out.println(text);
+
+        // Non-Local Variable access example
+        sub();
+
+        // Trying to call a variable from outside the SCOPE ... uncommenting and running will cause an error
+        // System.out.println(subText);
+
+    }
+
+
+
+    // SUB method for providing an example as to how external variables cannot be accessed (scope)
+    public static void sub()
+    {
+        String subText = "This is 'SUBTEXT' - it is local only to sub method";
+        System.out.println(subText);
+    }
+
+}
